40700b8e13dfde48bdd005398ccbb15ef62d2059
André Luiz Torres <andre.torres.ti@gmail.com>
2021-09-23 20:36:45 -0400

    Ajustes inicio projeto

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
-
-import './styles/index.scss'
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import "./styles/index.scss";
 
 render(
-    <React.StrictMode>
-        <Router>
-            <div>abc</div>
-        </Router>
-    </React.StrictMode>,
-    document.body
-)
+  <React.StrictMode>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />{console.log("oi")}
+        </Route>
+        <Route path="/login">
+          <Login />{console.log("oi denovo")}
+        </Route>
+      </Switch>
+    </Router>
+  </React.StrictMode>,
+  document.body
+);
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () : JSX.Element => {
+
+    return (
+        <div>abc</div>
+    )
+}
+
+export default Home;
